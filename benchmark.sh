8b2186ce485fda4e5957ac6a476c530ff4cd8878
Evan Ovadia <verdagon@gmail.com>
2023-03-27 20:44:47 -0400

    Initial version

---
+++ benchmark.sh
@@ -0,0 +1,10 @@
+$1 build ca=src --no_std true --region_override safe --output_dir build_safe
+$1 build ca=src --no_std true --region_override safe-baseline --output_dir build_safe_baseline
+$1 build ca=src --no_std true --region_override unsafe-fast --output_dir build_unsafe_no_bounds --include_bounds_checks false
+$1 build ca=src --no_std true --region_override unsafe-fast --output_dir build_unsafe_with_bounds
+
+# Do a run as a super-warmup
+hyperfine  "./build_safe/main" "./build_safe_baseline/main" "./build_unsafe_no_bounds/main" "./build_unsafe_with_bounds/main" --warmup 30 --runs 500 --export-csv summary.csv
+# Pay more attention to this one
+hyperfine  "./build_safe/main" "./build_safe_baseline/main" "./build_unsafe_no_bounds/main" "./build_unsafe_with_bounds/main" --warmup 30 --runs 500 --export-csv summary.csv
+
